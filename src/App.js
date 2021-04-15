0a3b7fc58c1d314db9e4ffa8f92a046abd3271e3
joselrdg <josesietepicos@gmail.com>
2021-04-16 01:26:06 +0200

    login

--- src/App.js
+++ src/App.js
@@ -1,8 +1,7 @@
-import { useState } from 'react';
 import { AppRouter } from './routers/AppRouter'
 
 function App() {
-  const [user, setUser] = useState(null)
+  
   return (
     <div className="App">
       <AppRouter />
--- src/services/BaseService.js
+++ src/services/BaseService.js
@@ -1,9 +1,20 @@
 import axios from 'axios';
+import { getAccessToken } from '../stores/AccessTokenStore';
 
 export const create = (opts = {}) => {
     const http = axios.create({
         baseURL: 'http://localhost:3001/api'
     })
+
+    http.interceptors.request.use(request => {
+        if(opts.useAccessToken!== false){
+            request.headers.common.Authorization = `Bearer ${getAccessToken()}`
+        } else {
+            delete request.headers.common.Authorization
+        }
+        return request
+    })
+
     http.interceptors.response.use(
         response => response.data
     )
