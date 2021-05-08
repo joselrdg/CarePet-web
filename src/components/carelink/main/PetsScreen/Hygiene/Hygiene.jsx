a85446588d20a3607d54bee8720c23294ce9f753
joselrdg <josesietepicos@gmail.com>
2021-05-08 11:21:19 +0200

    calendar

--- src/components/carelink/main/PetsScreen/Hygiene/Hygiene.jsx
+++ src/components/carelink/main/PetsScreen/Hygiene/Hygiene.jsx
@@ -1,10 +1,20 @@
 import { makeStyles, Paper } from '@material-ui/core';
 import React, { useEffect, useState } from 'react';
-import Pipebar from '../../../progress/Pipebar';
+import { Pipebar } from '../../../progress/Pipebar';
 import ConfirDialog from '../../../../common/ConfirDialog'
 import { usePet } from '../../../../hooks/usePet';
-import { getOnePet } from "../../../../../services/PetService";
+import { editPetUser } from "../../../../../services/PetService";
 
+import TodayIcon from '@material-ui/icons/Today';
+import { PieChart, Pie, Cell, } from "recharts";
+
+
+const data = [
+    { name: "Group A", value: 100 },
+    { name: "Group B", value: 2 },
+];
+// const COLORS = ['#0088FE', '#00C49F', '#FFBB28', '#FF8042'];
+const COLORS = ["#55f9aa", "#e91e63"];
 
 
 const useStyles = makeStyles((theme) => ({
@@ -36,33 +46,67 @@ export default function Hygiene() {
 
     const [render, setRender] = useState({
     })
-    const   lastwash= wash[wash.length - 1] ? wash[wash.length - 1] : { startDate: new Date(1995, 11, 17) }
-    const   willlastwash= willwash[willwash.length - 1] ? willwash[willwash.length - 1] : { startDate: new Date(1996, 12, 17) }
+    const lastwash = wash[wash.length - 1] ? wash[wash.length - 1] : { startDate: new Date(1995, 11, 17) }
+    const willlastwash = willwash[willwash.length - 1] ? willwash[willwash.length - 1] : { startDate: new Date(1996, 12, 17) }
 
-    const   lasthaircut= haircut[haircut.length - 1] ? haircut[haircut.length - 1] : { startDate: new Date(1995, 11, 17) }
-    const   willlasthaircut= willhaircut[willhaircut.length - 1] ? willhaircut[willhaircut.length - 1] : { startDate: new Date(1996, 12, 17) }
+    const lasthaircut = haircut[haircut.length - 1] ? haircut[haircut.length - 1] : { startDate: new Date(1995, 11, 17) }
+    const willlasthaircut = willhaircut[willhaircut.length - 1] ? willhaircut[willhaircut.length - 1] : { startDate: new Date(1996, 12, 17) }
 
-    const   lastearcleaning= earcleaning[earcleaning.length - 1] ? earcleaning[earcleaning.length - 1] : { startDate: new Date(1995, 11, 17) }
-    const   willlastearcleaning= willearcleaning[willearcleaning.length - 1] ? willearcleaning[willearcleaning.length - 1] : { startDate: new Date(1996, 12, 17) }
+    const lastearcleaning = earcleaning[earcleaning.length - 1] ? earcleaning[earcleaning.length - 1] : { startDate: new Date(1995, 11, 17) }
+    const willlastearcleaning = willearcleaning[willearcleaning.length - 1] ? willearcleaning[willearcleaning.length - 1] : { startDate: new Date(1996, 12, 17) }
 
-    const   lastteethcleaning= teethcleaning[teethcleaning.length - 1] ? teethcleaning[teethcleaning.length - 1] : { startDate: new Date(1995, 11, 17) }
-    const   willlastteethcleaning= willteethcleaning[willteethcleaning.length - 1] ? willteethcleaning[willteethcleaning.length - 1] : { startDate: new Date(1996, 12, 17) }
+    const lastteethcleaning = teethcleaning[teethcleaning.length - 1] ? teethcleaning[teethcleaning.length - 1] : { startDate: new Date(1995, 11, 17) }
+    const willlastteethcleaning = willteethcleaning[willteethcleaning.length - 1] ? willteethcleaning[willteethcleaning.length - 1] : { startDate: new Date(1996, 12, 17) }
 
-    const   lastvaccination= vaccination[vaccination.length - 1] ? vaccination[vaccination.length - 1] : { startDate: new Date(1995, 11, 17) }
-    const   willlastvaccination= willvaccination[willvaccination.length - 1] ? willvaccination[willvaccination.length - 1] : { startDate: new Date(1996, 12, 17) }
+    const lastvaccination = vaccination[vaccination.length - 1] ? vaccination[vaccination.length - 1] : { startDate: new Date(1995, 11, 17) }
+    const willlastvaccination = willvaccination[willvaccination.length - 1] ? willvaccination[willvaccination.length - 1] : { startDate: new Date(1996, 12, 17) }
 
-    const   lastdeworming= deworming[deworming.length - 1] ? deworming[deworming.length - 1] : { startDate: new Date(1995, 11, 17) }
-    const   willlastdeworming= willdeworming[willdeworming.length - 1] ? willdeworming[willdeworming.length - 1] : { startDate: new Date(1996, 12, 17) }
+    const lastdeworming = deworming[deworming.length - 1] ? deworming[deworming.length - 1] : { startDate: new Date(1995, 11, 17) }
+    const willlastdeworming = willdeworming[willdeworming.length - 1] ? willdeworming[willdeworming.length - 1] : { startDate: new Date(1996, 12, 17) }
 
+
+    const setPetSel = (pet) => {
+        setPetSelect(pet)
+    }
+
+    const datap = (thisstartDate, thisendDate) => {
+        // dias que llevamos
+        const fecha = new Date();
+        const fechaGetTime = fecha.getTime();
+
+        const lastdate = thisstartDate ? new Date(thisstartDate) : new Date();
+        const date = thisendDate ? new Date(thisendDate) : new Date();
+
+        let resta = date.getTime() - fechaGetTime;
+        const daysMas = Math.round(resta / (1000 * 60 * 60 * 24));
+
+        resta = fechaGetTime - lastdate.getTime();
+        // const daysMenos = Math.round(resta / (1000 * 60 * 60 * 24));
+
+
+        resta = date.getTime() - lastdate.getTime();
+        const total = Math.round(resta / (1000 * 60 * 60 * 24));
+        const percentage = (daysMas * 100) / total
+        return [
+            { name: "Group A", value: percentage },
+            { name: "Group B", value: 100 - percentage },
+        ];
+    }
+
+
+    console.log(lastwash.startDate)
+    console.log(willlastwash.startDate)
+    console.log(datap(lastwash.startDate, willlastwash.startDate))
 
     useEffect(() => {
-        console.log('render')
-        getOnePet(petSelect.id)
-        .then((p)=>{
-            setPetSelect(p)
-        })
-      }, [render]);
+        // console.log('render')
+        // getOnePet(petSelect.id)
+        // .then((p)=>{
+        //     setPetSelect(p)
+        // })
+    }, [render]);
 
+    const charwash = datap(lastwash.startDate, willlastwash.startDate)
     console.log(petSelect.id)
     return (
         <div>
@@ -72,10 +116,10 @@ export default function Hygiene() {
                     <Paper elevation={3} className={classes.weight}>
                         <div>
                             <i className="fas fa-bath fa-3x __mt-1 __mb-0 __mx-1 __icon-light"></i>
-                            <Pipebar startDate={lastwash.startDate} endDate={willlastwash.startDate} />
+                            <Pipebar data={datap(lastwash.startDate, willlastwash.startDate)} />
                         </div>
                         <Paper elevation={3} className={classes.weight}>
-                            <ConfirDialog render={render} setRender={setRender} id={petSelect.id} clave='wash' accion='último baño' />
+                            <ConfirDialog render={render} setPetSelect={setPetSelect} setRender={setRender} id={petSelect.id} clave='wash' accion='último baño' />
                         </Paper>
                     </Paper>
                 </div>
@@ -83,7 +127,7 @@ export default function Hygiene() {
                     <Paper elevation={3} className={classes.weight}>
                         <div>
                             <i className="fas fa-cut fa-3x __mt-1 __mb-0 __mx-1 __icon-light"></i>
-                            <Pipebar startDate={lasthaircut.startDate} endDate={willlasthaircut.startDate} />
+                            <Pipebar data={datap(lasthaircut.startDate, willlasthaircut.startDate)} />
                         </div>
                         <Paper elevation={3} className={classes.weight}>
                             <ConfirDialog id={petSelect.id} clave='haircut' accion='último corte' />
@@ -94,7 +138,7 @@ export default function Hygiene() {
                     <Paper elevation={3} className={classes.weight}>
                         <div>
                             <i className="fas fa-hand-sparkles fa-3x __mt-1 __mb-0 __mx-1 __icon-light"></i>
-                            <Pipebar startDate={lastearcleaning.startDate} endDate={willlastearcleaning.startDate} />
+                            <Pipebar data={datap(lastearcleaning.startDate, willlastearcleaning.startDate)} />
                         </div>
                         <Paper elevation={3} className={classes.weight}>
                             <ConfirDialog id={petSelect.id} clave='earcleaning' accion='limpieza de oídos' />
@@ -105,7 +149,7 @@ export default function Hygiene() {
                     <Paper elevation={3} className={classes.weight}>
                         <div>
                             <i className="fas fa-tooth fa-3x __mt-1 __mb-0 __mx-1 __icon-light"></i>
-                            <Pipebar startDate={lastteethcleaning.startDate} endDate={willlastteethcleaning.startDate} />
+                            <Pipebar data={datap(lastteethcleaning.startDate, willlastteethcleaning.startDate)} />
                         </div>
                         <Paper elevation={3} className={classes.weight}>
                             <ConfirDialog id={petSelect.id} clave='teethcleaning' accion='limpieza de dientes' />
@@ -116,7 +160,7 @@ export default function Hygiene() {
                     <Paper elevation={3} className={classes.weight}>
                         <div>
                             <i className="fas fa-syringe fa-3x __mt-1 __mb-0 __mx-1 __icon-light"></i>
-                            <Pipebar startDate={lastvaccination.startDate} endDate={willlastvaccination.startDate} />
+                            <Pipebar data={datap(lastvaccination.startDate, willlastvaccination.startDate)} />
                         </div>
                         <Paper elevation={3} className={classes.weight}>
                             <ConfirDialog id={petSelect.id} clave='vaccination' accion='última vacuna' />
@@ -127,7 +171,7 @@ export default function Hygiene() {
                     <Paper elevation={3} className={classes.weight}>
                         <div>
                             <i className="fas fa-bug fa-3x __mt-1 __mb-0 __mx-1 __icon-light"></i>
-                            <Pipebar startDate={lastdeworming.startDate} endDate={willlastdeworming.startDate} />
+                            <Pipebar data={datap(lastdeworming.startDate, willlastdeworming.startDate)} />
                         </div>
                         <Paper elevation={3} className={classes.weight}>
                             <ConfirDialog id={petSelect.id} clave='deworming' accion='desparasitación' />
--- src/components/carelink/progress/Pipebar.jsx
+++ src/components/carelink/progress/Pipebar.jsx
@@ -10,93 +10,43 @@ const data = [
 // const COLORS = ['#0088FE', '#00C49F', '#FFBB28', '#FF8042'];
 const COLORS = ["#55f9aa", "#e91e63"];
 
-export default class Pipebar extends PureComponent {
-  constructor(props) {
-    super(props);
-    this.state = {
-      data: props.datap,
 
-      addedAppointment: {},
-      appointmentChanges: {},
-      editingAppointment: undefined,
-    };
-    this.startDate = props.startDate;
-    this.endDate = props.endDate;
+
+
+export const Pipebar = ({ data }) => {
+  console.log(data)
+  if (!data){
+    return 'Loading...'
   }
-
-  datap = () => {
-    // dias que llevamos
-    const fecha = new Date();
-    const fechaGetTime = fecha.getTime();
-
-    const lastdate = this.startDate ? new Date(this.startDate) : new Date();
-    const date = this.endDate ? new Date(this.endDate) : new Date();
-
-    let resta = date.getTime() - fechaGetTime;
-    const daysMas = Math.round(resta / (1000 * 60 * 60 * 24));
-
-    resta = fechaGetTime - lastdate.getTime();
-    const daysMenos = Math.round(resta / (1000 * 60 * 60 * 24));
-
-
-    resta = date.getTime() - lastdate.getTime();
-    const total = Math.round(resta / (1000 * 60 * 60 * 24));
-    const percentage = (daysMas * 100) / total
-    return [
-      { name: "Group A", value: percentage },
-      { name: "Group B", value: 100 - percentage },
-    ];
-    // ({ percentage: percentage, rest:100 - percentage })
-
-
-
-    // const de = new Date(this.endDate)
-    // const ds = new Date(this.startDate)
-    // // const newd = new Date().getTime()
-    // // const sum = newd - de;
-    // // const total = ds - de;
-    // // const t = sum !== 0 ? (sum * 100) / total : 0
-
-
-    // const fecha = new Date();
-    // const restCurr = fecha.getTime() - ds.getTime();
-    // const restTotal = de.getTime() - ds.getTime();
-    // const days = Math.round(restCurr / (1000 * 60 * 60 * 24));
-    // const daysTotal = Math.round(restTotal / (1000 * 60 * 60 * 24));
-
-  }
-
-
-  render() {
-    console.log(this.datap())
-    return (
-      <div className='pipebar__root'>
-        <PieChart width={250} height={125} onMouseEnter={this.onPieEnter}>
-          <Pie
-            data={this.datap()}
-            cx={120}
-            cy={120}
-            startAngle={180}
-            endAngle={0}
-            innerRadius={80}
-            outerRadius={100}
-            fill="#8884d8"
-            paddingAngle={0}
-            dataKey="value"
-          >
-            {data.map((entry, index) => (
-              <Cell
-                key={`cell-${index}`}
-                fill={COLORS[index % COLORS.length]}
-              />
-            ))}
-          </Pie>
-        </PieChart>
-        <div className='pipebar__body'>
-          <TodayIcon />
-          <p>Fecha</p>
-        </div>
+  return (
+    <div className='pipebar__root'>
+      <PieChart width={250} height={125}
+      // onMouseEnter={this.onPieEnter}
+      >
+        <Pie
+          data={data}
+          cx={120}
+          cy={120}
+          startAngle={180}
+          endAngle={0}
+          innerRadius={80}
+          outerRadius={100}
+          fill="#8884d8"
+          paddingAngle={0}
+          dataKey="value"
+        >
+          {data.map((entry, index) => (
+            <Cell
+              key={`cell-${index}`}
+              fill={COLORS[index % COLORS.length]}
+            />
+          ))}
+        </Pie>
+      </PieChart>
+      <div className='pipebar__body'>
+        <TodayIcon />
+        <p>Fecha</p>
       </div>
-    );
-  }
+    </div>
+  );
 }
